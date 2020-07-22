db57e5687723d6e7392b9ef7a2fd76aadd09b23c
Tanja Veljanovska <tanja.marathon@gmail.com>
2020-07-22 10:46:15 +0200

    Making changes in the controller, views, routes and model

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,49 @@
 class TweetsController < ApplicationController
   def index
+    @tweets = Tweet.all
+  end
+
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+
+    if @tweet.save
+      redirect_to @tweet
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def update
+    @tweet = Tweet.find(params[:id])
+
+    if @tweet.update(tweet_params)
+      redirect_to @tweet
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+    redirect_to tweets_path
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:body)
   end
 end
+
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,3 @@
 class Tweet < ApplicationRecord
+  validates :body, presence: true, length: { minimum: 4 }
 end
