83899d1ec3968f652e3d8f120aa7ee291d4b5b65
Tanja Veljanovska <tanja.marathon@gmail.com>
2020-07-28 19:38:31 +0200

    Creating new controler for creating a new comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'tweets#index'
-  resources :tweets
+  resources :tweets do
+    resources :comments
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
