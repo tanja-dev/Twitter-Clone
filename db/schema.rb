1e8e48894d70dbfeda7aaf159fb361f8d13b3bf3
Tanja Veljanovska <tanja.marathon@gmail.com>
2020-08-04 18:52:56 +0200

    Generating user model, model validation, adding secure password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_25_152957) do
+ActiveRecord::Schema.define(version: 2020_08_04_161232) do
 
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
@@ -25,6 +25,15 @@ ActiveRecord::Schema.define(version: 2020_07_25_152957) do
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "comments", "tweets"
