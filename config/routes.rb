e00c2bd8002cc66ef57e2d070243dd543bd9ba90
Tanja Veljanovska <tanja.marathon@gmail.com>
2020-08-04 19:48:11 +0200

    Generating session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'tweets#index'
   resources :tweets do
     resources :comments
   end
 
+  get 'users/signup', to: 'users#new'
+  resources :users, except: :new
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
